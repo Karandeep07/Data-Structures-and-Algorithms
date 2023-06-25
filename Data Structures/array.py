c900631595bd12ce6ca24c27a3b8c9382df94c69
Karandeep Padam <41012770+Karandeep07@users.noreply.github.com>
2023-06-25 12:32:47 +0530

    Add array.py

---
+++ Data Structures/array.py
@@ -0,0 +1,40 @@
+# Arrays are one of the most commonly-used data structures
+# The elements of an array are stored in contiguous memory locations
+# Arrays are of two types : Static and Dynamic
+# Static arrays have fixed, pre-defined amount of memory that they can use, whereas in dynamic arrays this is flexible
+# In Python we only have dynamic arrays
+
+arr = ['a', 'b', 'c', 'd']
+print(arr[2])
+
+# push
+arr.append('e')  # O(1)
+print(arr)
+
+# In some special cases, the append(push) operation may take greater time. This is because as mentioned earlier, Python has dynamic arrays
+# So when an element is to appended and the array is filled, the entire array has to be copied to a new location
+# With more space allocated(generally double the space) this time so that more items can be appended.
+# Therefore, some individual operations may require O(n) time or greater, but when averaged over a large number of operations,
+# The complexity can be safely considered to be O(1)
+
+# pop
+arr.pop()  # O(1)
+print(arr)  # Removes last element
+
+# insert
+# insert anywhere in array
+# Syntax
+arr.insert(0, 'x')  # O(n)
+print(arr)
+
+# Delete
+arr.pop(0)  # Pops the first element of the array. O(n)
+print(arr)
+
+# Removes the first occurrence of the element 'c' in the array. O(n)
+arr.remove('c')
+print(arr)
+
+arr1 = ['a', 'b', 'c', 'd']
+del arr1[1:3]  # Deletes elements from position 1 to position 2. O(n)
+print(arr1)
---
+++ Interview Questions/Question1.py
@@ -0,0 +1,16 @@
+# We are given two arrays. We have to find if these two arrays contain any matching elements.
+# For example, array1 = ['a','b','c','x'] , array2 = ['x','y','z']
+# This should return True as element 'x' appears in both arrays.
+# Example 2: array1 = ['a','b','c'] , array2 = ['x','y'] => False
+
+# 2 inputs = 2 arrays - no size limit
+# o/p: T or F
+
+# Brute force approach
+# loop through array1 and loop through array2 comparing each element of a2 with a1
+# O(n^2)
+# But array size can be different so Time complexity will be
+# O(a*b)
+
+
+
