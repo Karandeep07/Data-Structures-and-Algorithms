c71f57ae94b2b215ddb849d47b4da2126629de24
Karandeep Padam <41012770+Karandeep07@users.noreply.github.com>
2023-06-28 10:03:19 +0530

    Update Implementing array.py

--- Data Structures/Implementing array.py
+++ Data Structures/Implementing array.py
@@ -3,3 +3,38 @@
 
 # Here, we will implement our own array with some common methods such as access, push, pop, insert, delete
 
+class Array():
+    def __init__(self):
+        self.length = 0
+        self.data = {}
+
+    # The __str__ method is called when the following functions are invoked on the object and return a string:
+    # print()
+    # str()
+
+    def __str__(self):
+        return str(self.__dict__)
+    
+    def get(self, index):
+        return self.data[index]
+    
+    def push(self, item):
+        self.data[self.length] = item
+        self.length += 1
+
+    def pop(self):
+        last_item = self.data[self.length - 1]
+        del self.data[self.length - 1]
+        self.length -= 1
+        return last_item
+
+# Use  print() for output
+
+arr=Array()
+print(arr) # {'length': 0, 'data': {}}
+
+arr.push('hi')
+print(arr) # {'length': 1, 'data': {0: 'hi'}}
+
+print(arr.pop()) # hi
+print(arr)   # {'length': 0, 'data': {}}
