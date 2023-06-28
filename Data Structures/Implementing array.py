cb1dd3515d358ea2cb0f38d12df7ee16adfcab4e
Karandeep Padam <41012770+Karandeep07@users.noreply.github.com>
2023-06-28 10:30:09 +0530

    Update Implementing array.py

--- Data Structures/Implementing array.py
+++ Data Structures/Implementing array.py
@@ -1,5 +1,5 @@
 # Implementation
-    # Although arrays are pre-defined in Python in the form of lists, we can implement our own arrays.
+# Although arrays are pre-defined in Python in the form of lists, we can implement our own arrays.
 
 # Here, we will implement our own array with some common methods such as access, push, pop, insert, delete
 
@@ -14,10 +14,10 @@ class Array():
 
     def __str__(self):
         return str(self.__dict__)
-    
+
     def get(self, index):
         return self.data[index]
-    
+
     def push(self, item):
         self.data[self.length] = item
         self.length += 1
@@ -28,13 +28,26 @@ class Array():
         self.length -= 1
         return last_item
 
+    def delete(self, index):
+        for i in range(index, self.length-1):
+            self.data[i] = self.data[i+1]
+        del self.data[self.length - 1]
+        self.length -= 1
+
 # Use  print() for output
 
-arr=Array()
-print(arr) # {'length': 0, 'data': {}}
+arr = Array()
+print(arr)  # {'length': 0, 'data': {}}
 
 arr.push('hi')
-print(arr) # {'length': 1, 'data': {0: 'hi'}}
+print(arr)  # {'length': 1, 'data': {0: 'hi'}}
 
-print(arr.pop()) # hi
+print(arr.pop())  # hi
 print(arr)   # {'length': 0, 'data': {}}
+
+arr.push('yo')
+arr.push('!')
+print(arr)
+
+arr.delete(1)
+print(arr)
